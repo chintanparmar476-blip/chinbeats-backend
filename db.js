07f221af1234488a267498e5ba2270f9196be486
Chintan Parmar <chintanparmar476@gmail.com>
2026-03-27 23:14:29 +0530

    fix db direct

--- db.js
+++ db.js
@@ -1,13 +1,22 @@
 const mysql = require("mysql2");
-require("dotenv").config();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  host: "gondola.proxy.rlwy.net",
+  user: "root",
+  password: "ZZGsZfsDqGaxcZkXbUriDfpfzPHuaumn",
+  database: "railway",
+  port: 11774
 });
+
+db.connect((err) => {
+  if (err) {
+    console.log("❌ Database connection failed:", err);
+  } else {
+    console.log("✅ MySQL connected");
+  }
+});
+
+module.exports = db;
 
 db.connect((err) => {
   if (err) {
