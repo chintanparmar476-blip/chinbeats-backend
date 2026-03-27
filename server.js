214293c5f9116d22921b386cff78a63dff6dcc0e
Chintan Parmar <chintanparmar476@gmail.com>
2026-03-27 22:02:25 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+const db = require("./db");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend running 🚀");
+});
+
+// REGISTER
+app.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  const hashed = await bcrypt.hash(password, 10);
+
+  db.query(
+    "INSERT INTO users (username,email,password) VALUES (?,?,?)",
+    [username, email, hashed],
+    (err) => {
+      if (err) return res.json({ message: "User exists" });
+      res.json({ message: "Registered successfully" });
+    }
+  );
+});
+
+// LOGIN
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+
+  db.query("SELECT * FROM users WHERE email=?", [email], async (err, result) => {
+    if (result.length === 0)
+      return res.json({ message: "User not found" });
+
+    const user = result[0];
+    const match = await bcrypt.compare(password, user.password);
+
+    if (match) {
+      res.json({ message: "Login successful", user });
+    } else {
+      res.json({ message: "Wrong password" });
+    }
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running 🚀");
+});
